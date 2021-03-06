b8e933e0481dbc5eb784d78397cf001d7ae41061
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2021-03-06 17:27:42 -0300

    Criação do componente de Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Input.module.css'
+
+export default function Input(props) {
+  return (
+    <div className={styles.container}>
+      <label htmlFor={props.id}>{props.placeholder}</label>
+      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+    </div>
+  )
+}
