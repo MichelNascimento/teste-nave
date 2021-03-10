2687e1439a10c35a0a70036da8474c64ee915974
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2021-03-09 22:12:04 -0300

    Implementação da listagem de Navers

---
+++ src/components/ListNavers/index.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import Button from '../Button'
+import CardNaver from '../CardNaver'
+
+import api from '../../services/api'
+import styles from './ListNavers.module.css'
+
+export default function ListNavers() {
+  const [userInfo, setUserInfo] = useState([])
+
+  const token = localStorage.getItem('token')
+
+  async function loadNavers() {
+    try {
+      await api.get('/navers', {
+        headers: {
+          Authorization: token
+        }
+      }).then(response => {
+        setUserInfo(response.data)
+      })
+
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    loadNavers()
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.ListNaversContainer}>
+        <div className={styles.title}>
+          <h1>Navers</h1>
+          <Link to="/NaverRegister" text="Adicionar Naver">
+            Adicionar naver
+          </Link>
+        </div>
+        <div className={styles.listNavers}>
+          {userInfo.map(user => (
+            <CardNaver
+              key={user.id}
+              urlPhoto={user.url}
+              name={user.name}
+              jobRole={user.job_role}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
